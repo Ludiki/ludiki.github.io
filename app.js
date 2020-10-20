bd22f92f5185166bd01827a00c85d00114d8734f
Ludiki <42914643+Ludiki@users.noreply.github.com>
2020-10-20 23:22:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,54 +1,19 @@
-// Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
-// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger"),
-    cameraOverlay = document.querySelector("#camera--overlay")
-// Access the device camera and stream to cameraView
-function cameraStart() {
+var constraints = { video: {facing: "environment"}, audio: false};
+
+// constants
+const cameraView = document.querySelector("#camera--view");
+
+function cameraStart(){
     navigator.mediaDevices
         .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
-    overlay();
-}
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
-
-// own
-function overlay(){
-    var ctx = cameraOverlay.getContext("2d")
-    dpi = window.devicePixelRatio;
-
-    let style = {
-        height() {
-          return +getComputedStyle(canvas).getPropertyValue('height').slice(0,-2);
-        },
-        width() {
-          return +getComputedStyle(canvas).getPropertyValue('width').slice(0,-2);
-        }
-    }
-    canvas.setAttribute('width', style.width() * dpi);
-    canvas.setAttribute('height', style.height() * dpi);
-
-    ctx.beginPath();
-    ctx.arc(100, 100, 40, 0, 2 * Math.PI);
-    ctx.stroke();
+        .then(function(stream){
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error){
+            console.error("Something wrong");
+        })
 }
 
-
-// Start the video stream when the window loads
+// Start video stream
 window.addEventListener("load", cameraStart, false);
