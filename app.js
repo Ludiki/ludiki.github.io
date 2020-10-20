bf3e3e126576c9aa46619a4f9b5a41360fa783d4
Ludiki <42914643+Ludiki@users.noreply.github.com>
2020-10-20 16:56:55 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
-// Set constraints for the video stream
-var constraints = { video: { facingMode: "environment" }, audio: false };
-// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
+//check for device support
+if("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices){
+    //browser supports camera
 
-// Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
+    // video requirements
+    const constraints ={
+        video: {
+            width: {
+              min: 1280,
+              ideal: 1920,
+              max: 2560,
+            },
+            height: {
+              min: 720,
+              ideal: 1080,
+              max: 1440,
+            },
+        }
+    }
+
+
+    // capture the video stream
+    const videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+
+
+
 }
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
-  document.getElementById("overlay").style.display = "block";
