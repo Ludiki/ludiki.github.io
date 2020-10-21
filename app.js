ea85480924de3128b111e52cf6287c52cb3da050
Ludiki <42914643+Ludiki@users.noreply.github.com>
2020-10-21 14:40:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var video = document.getElementById("video");
+// Grab elements, create settings, etc.
+var video = document.getElementById('video');
 
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
-    navigator.mediaDevices.getUserMedia({video: true}).then(function(stream){
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
-        video.onplay();
-    })
+        video.play();
+    });
 }
