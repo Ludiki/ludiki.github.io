a3a5c8dc03f6317587e26e7025b16c417ecf298d
Ludiki <42914643+Ludiki@users.noreply.github.com>
2020-10-23 16:00:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+var video = document.getElementById('video');
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
 // Put event listeners into place
 window.addEventListener("DOMContentLoaded", function() {
+  ctx.moveTo(0,0);
+  ctx.lineTo(1000,1000);
+  ctx.stroke();
     // Grab elements, create settings, etc.
-    var video = document.getElementById('video');
-    var mediaConfig =  { video: {facingMode:"environment"} };
+    var mediaConfig = {
+        video: {
+          width: {
+            min: 1280,
+            ideal: 1920,
+            max: 2560,
+          },
+          height: {
+            min: 720,
+            ideal: 1080,
+            max: 1440
+          },
+          facingMode: 'environment'
+        }
+    }
+
     var errBack = function(e) {
         console.log('An error has occurred!', e)
     };
